ab9439f5592cabdfed259651bd5892c5092f4fbb
Gianluca Mazzeo <g.mazzeo@theinnovationfactory.it>
2024-07-18 09:23:55 +0200

    added .env.local

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '995lo464',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: 'production'
   }
 })
